7a9f1e040485b51d5a73883a03bf72cd9441ed6a
MaartenVanDenGoor <maarte626@gmail.com>
2018-05-13 17:27:40 +0200

    goede versie

--- MaartenOCP/src/main/java/nl/maarten/ocp/m4streams/Faculteit100.java
+++ MaartenOCP/src/main/java/nl/maarten/ocp/m4streams/Faculteit100.java
@@ -1,62 +1,19 @@
 package nl.maarten.ocp.m4streams;
 
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.*;
 import java.math.*;
 
 public class Faculteit100 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Collector<BigInteger, BigInteger, BigInteger> factCollector =
-			    Collector.of(() -> BigInteger.ONE              // supplier
-			                ,(x, y) -> x.multiply(y)           // accumulator
-			                ,(x2, y2) -> x2.multiply(y2));       // combiner
-			                //,BigInteger::toString);            // finisher
-	    /*
-		
 		String resultaat = IntStream.rangeClosed(1, 100)
-				                 .parallel()
-				                 .mapToObj(BigInteger::valueOf)
-				                 .collect(() -> BigInteger.ONE   //Supplier
-				                         ,BigInteger::multiply   //Accumulator
-				                         ,BigInteger::multiply)  //combiner
-    			                 .toString();
-		System.out.println(resultaat);
-		String resultaat = IntStream.rangeClosed(1, 7)
-                .parallel()
-                .mapToObj(MyBigInteger::new)
-                .collect(MyBigInteger::new  //Supplier
-                        ,(a, b) -> a.set(b.get())  //Accumulator
-                        ,(a, b) -> a.multiply(b))  //combiner
-                .toString();
-		*/		
-		String resultaat = IntStream.rangeClosed(1, 17)
-                .parallel()
-                .mapToObj(BigInteger::valueOf)
-                .collect(MyBigInteger::new  //Supplier
-                        ,MyBigInteger::add  //Accumulator
-                        ,MyBigInteger::multiply)  //combiner
-                .toString();		
+                                    .parallel()
+                                    .mapToObj(BigInteger::valueOf)
+                                    .collect(MyBigInteger::new         //Supplier
+                                            ,MyBigInteger::multiply    //Accumulator
+                                            ,MyBigInteger::combine)    //combiner
+                                    .toString();		
         System.out.println("resultaat "+resultaat);		
 		System.out.println("faculteit "+faculteit(7));
-       /*
-        BigInteger res = IntStream.rangeClosed(1, 100)
-                           //  .parallel()
-                             .mapToObj(BigInteger::valueOf)
-                             .collect(factCollector);		
-
-		System.out.println(res);
-
-		BigInteger big = BigInteger.valueOf(3);
-		System.out.println(big);
-		big.multiply(BigInteger.valueOf(3));
-		System.out.println(big);
-		resultaat = IntStream.rangeClosed(1, 5)
-                             .collect(() -> Integer(0)   //Supplier
-                                     ,(x,y) -> x*y      //Accumulator
-                                     ,(x,y) -> x*y )  //combiner		
-        System.out.println(resultaat);                
-        */
 	}
 	public static long faculteit (long p) {
 		if (p == 1) return 1;
@@ -64,15 +21,14 @@ public class Faculteit100 {
 	}
 }
 class MyBigInteger {
-	private BigInteger big = BigInteger.ZERO;
+	private BigInteger big = BigInteger.ONE;
 	
 	MyBigInteger () {
 	}
-	public void add (BigInteger p) {
-		big = big.add(p);
+	public void multiply (BigInteger p) {
+		big = big.multiply(p);
 	}
-	public void multiply (MyBigInteger p) {
-		System.out.println(big + " * " + p);
+	public void combine (MyBigInteger p) {
 		big = big.multiply(p.big);
 	}
 	public String toString() {
