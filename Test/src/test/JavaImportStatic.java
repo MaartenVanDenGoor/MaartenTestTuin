989b1bf4a6a9c6cb3c7c8e47060695970b738967
MaartenVanDenGoor <maarte626@gmail.com>
2018-07-16 21:37:41 +0200

    meer

--- Test/src/test/JavaImportStatic.java
+++ Test/src/test/JavaImportStatic.java
@@ -1,6 +1,8 @@
 package test;
 import java.util.List;
 import static java.util.Arrays.asList;  // static import
+
+import java.io.IOException;
 import java.util.ArrayList;
 public class JavaImportStatic {
 	public static void main(String[] args) {
@@ -8,10 +10,30 @@ public class JavaImportStatic {
 		for (String s:lijst) {
 			System.out.println(s);   // One
 		}
+		fly(1);
 		}
 	public static List<String> asList(String s){
 		ArrayList<String> l = new ArrayList<String>();
 		l.add("One");
 		return l;
+	}
+//public static void fly(int parm) {
+//	System.out.println("int");
+//}
+public static void fly(Object parm) {
+	System.out.println("object");
+}
+public static void fly(int... parm) {
+	System.out.println("int...");
+}
+//public static void fly(Integer parm) {
+//	System.out.println("Integer");
+//}
+}
+class Bunny {
+	Bunny(int heigth) {
+		this(heigth,"brown");  
+	}   
+	Bunny(int heigth, String  color){
 	}
 }
